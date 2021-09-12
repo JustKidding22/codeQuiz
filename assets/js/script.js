780725909e4d902a5168026ccdc13f9ae11249a9
James K <projdk90@gmail.com>
2021-09-11 19:27:42 -0700

    redo front page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,25 @@
-// Dom Methods Here ! 
-
 var body = document.body;
 
-// View High Score
-var aEl = document.createElement('a-href');
-aEl.textContent = 'View High Score';
-aEl.style.fontFamily = 'Arial, Helvetica, sans-serif';
-body.appendChild(aEl);
+
+
+// High Scores
+var h4El = document.createElement('h4');
+h4El.textContent = 'High Scores :';
+h4El.setAttribute('id', 'high-score');
+h4El.style.fontFamily = 'Arial, Helvetica, sans-serif';
+body.appendChild(h4El);
 
 // Timer
-var timerDis = document.createElement('p');
-timerDis.textContent = 'Time: 0';
-timerDis.style.fontFamily = 'Arial, Helvetica, sans-serif';
-body.appendChild(timerDis);
+var h4Timer = document.createElement('h4');
+h4Timer.textContent = 'Time :';
+h4Timer.setAttribute('id', 'timer-display');
+h4Timer.style.fontFamily = 'Arial, Helvetica, sans-serif'
+body.appendChild(h4Timer);
 
 // Quiz 
 var h1El = document.createElement('h1');
 h1El.textContent = 'Coding Quiz Challenge';
+h1El.setAttribute('id', 'front-header');
 h1El.setAttribute('style', 'margin:auto; width:100%; text-align:center; padding:20px');
 h1El.style.fontFamily = 'Arial, Helvetica, sans-serif';
 body.appendChild(h1El);
@@ -25,6 +28,7 @@ body.appendChild(h1El);
 var pEl = document.createElement('p');
 pEl.textContent =
   'Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!';
+pEl.setAttribute('id', 'front-text');
 pEl.setAttribute('style', 'margin:auto; width:100%; text-align:center; padding-bottom:20px');
 pEl.style.fontFamily = 'Arial, Helvetica, sans-serif';
 body.appendChild(pEl);
@@ -32,64 +36,62 @@ body.appendChild(pEl);
 // Start Quiz Button
 var btnEl = document.createElement('button');
 btnEl.innerHTML = "Start Quiz";
+btnEl.setAttribute('id', 'startBtn');
 btnEl.setAttribute('style', 'margin:auto; display:flex; border-radius:4px; background: mediumpurple; color:white;');
 btnEl.style.fontFamily = 'Arial, Helvetica, sans-serif';
 document.body.appendChild(btnEl);
 
 
+// Create Variable for timer
+
+// onclick Event
+btnEl.onclick = function() {
+  var time = 0
+  setInterval(function() {
+    time ++
+    console.log(time);
+  }, 1000)
+};
+
+// Shows questions in array
 
 
 
-// Array for Questions for Quiz
-var myQuestions = [
+// See the choices
+
+
+
+
+
+
+
+
+var myQuestions = 
+[
   {
-    question: "What is the capital of Nevada?",
-    answers: {
-      a: "Carson City",
-      b: "Las Vegas",
-      c: "Reno",
-      d: "Sparks"
-    },
-    correctAnswer: "a"
+    question: "What is the capital of Neveda?",
+    choices: ["Carson City", "Las Vegas", "Reno", "Sparks"],
+    answer: 1
   },
+
   {
     question: "What is the capital of California?",
-    answers: {
-      a: "Los Angeles",
-      b: "Sacramento",
-      c: "San Diego",
-      d: "San Francisco"
-    },
-    correctAnswer: "b"
+    choices: ["Los Angeles", "Sacramento", "San Diego", "San Francisco"],
+    answer: 2
   },
   {
     question: "What is the capital of Texas?",
-    answers: {
-      a: "Dallas",
-      b: "Houston",
-      c: "Austin",
-      d: "San Antonio"
-    },
-    correctAnswer: "c"
+    choices: ["Dallas", "Houston", "Austin", "San Antonio"],
+    answer: 3
   },
   {
     question: "What is the capital of Alaska?",
-    answers: {
-      a: "Anchorage",
-      b: "Fairbanks",
-      c: "Ketchikan",
-      d: "Juneau"
-    },
-    correctAnswer: "d"
+    choices: ["Anchorage", "Fairbanks", "Ketchikan", "Juneau"],
+    answer: 4
   },
   {
     question: "What is the capital of Arizona?",
-    answers: {
-      a: "Phoenix",
-      b: "Tucson",
-      c: "Mesa",
-      d: "Scottsdale"
-    },
-    correctAnswer: "a"
-  }
+    choices: ["Phoenix", "Tucson", "Mesa", "Scottsdale"],
+    answer: 1
+  },
 ];
